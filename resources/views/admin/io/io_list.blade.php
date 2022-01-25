c7a89c50e73a0475844b88f76a62ec471fbfa8f1
David C <dchincharashvili@archive.gov.ge>
2022-01-25 13:51:41 +0400

    added add button component

--- resources/views/admin/io/io_list.blade.php
+++ resources/views/admin/io/io_list.blade.php
@@ -49,12 +49,7 @@
     </tbody>
 </table>
 
-
-
-<div style="position:fixed; right: 100px; bottom: 100px; border-radius: 50%; background-color: #00fa9a; padding: 5px; border: 1px solid black; ">
-    <a href="{{route("io.add")}}"> <span class="material-icons md-light">add</span> </a>
-</div>
-
+<x-add-button route="{{route('io.add')}}"></x-add-button>
 
 <script>
 
--- resources/views/admin/iotypes/type_list.blade.php
+++ resources/views/admin/iotypes/type_list.blade.php
@@ -35,7 +35,6 @@
     </table>
 
 
-    <div style="position:fixed; right: 100px; bottom: 100px; border-radius: 50%; background-color: #00fa9a; padding: 5px; border: 1px solid black; ">
-        <a href="{{route("types.add")}}"> <span class="material-icons md-light">add</span> </a>
-    </div>
+    <x-add-button route="{{route('types.add')}}"></x-add-button>
+
 @endsection
